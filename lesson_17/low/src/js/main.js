4324b05c45ffd80c92779a843d240f9f0953282a
Ilya-Kuznezov <galavalomca@gmail.com>
2019-08-06 16:29:34 +0500

    готово но без проверки

--- lesson_17/low/src/js/main.js
+++ lesson_17/low/src/js/main.js
@@ -1,11 +1,16 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
+var close2 = document.querySelector('#close2');
 
 button.addEventListener('click', function(){
   modal.classList.add('modal_active');
 });
 
 close.addEventListener('click', function() {
+  modal.classList.remove('modal_active');
+})
+
+close2.addEventListener('click', function() {
   modal.classList.remove('modal_active');
 })
